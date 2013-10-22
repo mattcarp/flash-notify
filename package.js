3240fda2bbb2798c546c8eed64ed66d45c5ae73e
Matt Carpenter <matt@mattcarpenter.com>
2013-10-22 09:57:37 +0100

    added notifications

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-    summary: "a means of displaying messages to the user"
+    summary: "for sending flash messages and persistent notifications to the user"
 });
 
 Package.on_use(function (api, where) {
@@ -11,25 +11,38 @@ Package.on_use(function (api, where) {
       ], 'client'
       );
 
+
     api.add_files([
-      'lib/flashes.js',
+      'lib/flash_notify_server.js'
+      ], 'server'
+    );
+
+    api.add_files([
+      'lib/flash_notify_client.js',
       'lib/flashes_list.html',
+      'lib/notifications.html',
+      'lib/notifications.js',
       'lib/flashes_list.js',
+      'lib/css/flash_notify.css',
       'lib/gritter.min.js',
       'lib/images/gritter.png',
-      'lib/css/gritter.css',
-      'lib/jquery_modal.min.js',
-      'lib/css/jquery_modal.css'
+      'lib/css/gritter.css'
       ], 'client'
     );
 
+    api.add_files([
+      'lib/flash_notify_common.js'
+      ], ['server', 'client']
+    );
+
+
     if (api.export)
-        api.export('Flashes');
+      api.export('FlashNotify');
 });
 
 Package.on_test(function(api) {
-  api.use('flashes', 'client');
+  api.use('flash-notify', 'client');
   api.use(['tinytest', 'test-helpers'], 'client');
 
-  api.add_files('flashes_tests.js', 'client');
+  api.add_files('flash-notify_tests.js', 'client');
 });
